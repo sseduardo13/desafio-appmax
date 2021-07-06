62c926c7d5d2a5b9f1c67c24508a82174311719c
eduardo.santos <eduardo.santos@pulses.com.br>
2021-07-06 00:15:20 -0300

    Cria index de produtos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+
 class ProductController extends Controller
 {
     /**
@@ -13,7 +15,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+
+        dd($products);
     }
 
     /**
