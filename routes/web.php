fafef7d32c72ecc82a002a45dfcd23c5166bd09f
eduardo.santos <eduardo.santos@pulses.com.br>
2021-07-06 18:41:15 -0300

    Cria controller, model e rota para estoque

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StockController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/products', ProductController::class);
+Route::resource('products', ProductController::class);
+
+Route::resource('stock', StockController::class);
