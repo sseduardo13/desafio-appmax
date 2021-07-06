9b1113aa8e9b198105d035c9c2fdb0d842b9b07c
eduardo.santos <eduardo.santos@pulses.com.br>
2021-07-06 18:34:35 -0300

    Cria tabela de estoque do produto

---
+++ database/migrations/2021_07_06_212837_create_stock_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_product');
+            $table->string('client', 255);
+            $table->string('operation');
+            $table->string('origin');
+            $table->integer('amount');
+            $table->timestamps();
+            $table->foreign('id_product')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+}
